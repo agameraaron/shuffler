ee36e8249b0fa6a73669c1896cfa2810ec07a235
Aaron M <agameraaron@gmail.com>
2018-04-08 23:15:14 -0600

    Version 0.2
    It has now been converted to work in Godot Engine 3.0 . The shuffling algorithm has been adjusted so it should always have at least some deceleration and therefore no longer be able to shuffle forever on accident.
    
    [Note: the commit right before this was false in that I accidentally reuploaded version 0.1! I decided it was inconsequential to just upload the right files without reverting back the head since nothing changed anyways.]

--- main.gd
+++ main.gd
@@ -1,6 +1,9 @@
 extends Node2D
 
+var version = "0.2"
+
 var program_state = "main"
+
 var shuffle_state = "done"
 
 var hold_time = 0
@@ -19,16 +22,16 @@ var entries = ["these","are","tests","and","examples"]
 
 
 func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here
-	set_fixed_process(true)
+	#update the about header with the version
+	get_node("overlay frame/about panel/about header").set_text("How to use:\n1. Access the list setup by pressing the bottom-right most button.\n2. Add items as desired to the list that you want shuffled.\n3. Hold the shuffle button for more power, then let go and it will shuffle the results!\n\nAbout:\nVersion "+version+",  Created by AGamerAaron of \"VAR Team\".\nMIT licensed. Feel free to contribute on Github!")
 	
 	
 
-func _fixed_process(delta):
-	
+func _physics_process(delta):
+	#print(program_state)
 	if program_state == "main":
 		main_frame(delta)
+		#print("hi")
 	elif program_state == "settings":
 		pass
 	elif program_state == "list setup":
@@ -40,12 +43,15 @@ func _fixed_process(delta):
 
 
 func new_entry():
-	if get_node("new entry frame/new entry input").has_focus() and ( Input.is_key_pressed(KEY_RETURN) or Input.is_key_pressed(KEY_ENTER) or Input.is_key_pressed(KEY_KP_ENTER) ):
+	if get_node("new entry frame/new entry input").has_focus() and (Input.is_key_pressed(KEY_ENTER) or Input.is_key_pressed(KEY_KP_ENTER)):
 		_on_confirm_button_pressed()
 		
 		
 
 func main_frame(delta):
+	
+	
+	#print(shuffle_state)
 	
 	if shuffle_state == "initialize":
 		
@@ -62,28 +68,33 @@ func main_frame(delta):
 		#size of the entries list
 		max_shuffle_number = get_node("list setup frame/entry list").get_item_count()#entries.size()
 		
-		if max_shuffle_number <= 1:
+		if max_shuffle_number <= 0:
 			shuffle_state = "done"
 			get_node("overlay frame").show()
 			get_node("overlay frame/setup list warning panel").show()
 			get_node("overlay frame/setup list warning panel/setup list warning confirm").grab_focus()
 		else:
-			print("max: "+str(max_shuffle_number))
+			print("Maximum number based on the amount of entries: "+str(max_shuffle_number))
 			
 			if hold_time > 100:
 				hold_time = 100
-			
-			#find the difference between 100 & hold time
-			var inversed_hold_time = 100 - hold_time
+			print("hold time: "+str(hold_time))
+			#find the difference between the max power & hold time
+			var inversed_hold_time = 110 - hold_time
 			
 			
 			#the greater the hold time the less resistance
-			resistance = inversed_hold_time *0.005
-			
+			resistance = (inversed_hold_time *0.005) - 0.08
+			if resistance <= 0.01:
+				resistance = 0.01
 			
 			shuffle_state = "active"
 	
-	
+	if shuffle_state == "pressed":
+		#reverses direction when maxed and vice versa
+		meter_direction()
+		
+		#print(str(round(next_number)))
 	
 	#Shuffling active loop
 	if shuffle_state == "active":
@@ -108,19 +119,21 @@ func main_frame(delta):
 			while next_number >= (max_shuffle_number-1):
 				#print("meep: "+str(next_number))
 				next_number = next_number - (max_shuffle_number-1)
-			print("next number: "+str(next_number))
+			#print("next number: "+str(round(next_number)))
 			
-			
+		
+		#display the next number
+		get_node("main frame/number result").set_text(str(round(abs(next_number))))
+		#display the next entry
+		#print(get_node("list setup frame/entry list").get_item_text(int(round(next_number))))
+		get_node("main frame/result").set_text(get_node("list setup frame/entry list").get_item_text(int(round(abs(next_number)))) )#(entries[next_number-1])
+		
+		
 	
-	#reverses direction whe maxed and vice versa
-	meter_direction()
 	
 	
-	#display the next number
-	get_node("main frame/number result").set_text(str(round(next_number)))
-	#display the next entry
-	#print(get_node("list setup frame/entry list").get_item_text(int(round(next_number))))
-	get_node("main frame/result").set_text( get_node("list setup frame/entry list").get_item_text(int(round(next_number))) )#(entries[next_number-1])
+	
+	
 	
 	
 	
@@ -129,24 +142,31 @@ func main_frame(delta):
 	
 	#update power meter display
 	if shuffle_state == "pressed":
-		
-		get_node("main frame/power meter").set_value(hold_time)
-		
-		#If at full power, change color to red, otherwise keep it green
-		if get_node("main frame/power meter").get_value() >= 100:
+		if entries.size() > 0:
 			
-			#print(get_node_and_resource("res://main.tscn::5"))
-			#print(get_node_and_resource("main frame/power meter"))
-			#print(ResourceInteractiveLoader.get_resource("res://main.tscn::5"))
-			#var new_color = Color.new()
-			#new_color = 
-			#get_node("main frame/power meter").add_color_override("max",Color(128,12,52))#.set_bg_color( Color(255,255,0) )  )   )
 			
-			#I give up for now
-			pass
+			#If at full power, change color to red, otherwise keep it green
+			#if get_node("main frame/power meter").value >= 100: #get_node("main frame/power meter").get_value() >= 100:
+				
+				#print(get_node_and_resource("res://main.tscn::5"))
+				#print(get_node_and_resource("main frame/power meter"))
+				#print(ResourceInteractiveLoader.get_resource("res://main.tscn::5"))
+				#var new_color = Color.new()
+				#new_color = 
+				#get_node("main frame/power meter").add_color_override("max",Color(128,12,52))#.set_bg_color( Color(255,255,0) )  )   )
+				
+				#I give up for now
+			#	pass
+			#else:
+				#hold_time = hold_time + 1
+			get_node("main frame/power meter").value = hold_time #get_node("main frame/power meter").set_value(hold_time)
 			
 		else:
-			pass
+			#setup_list_warning_active = true
+			#get_node("overlay frame").show()
+			#get_node("overlay frame/setup list warning panel").show()
+			print("Error! No entries!")
+			
 	
 	
 	#update all debug displays
@@ -186,15 +206,18 @@ func update_debug():
 
 func _on_shuffle_button_input_event( event ):
 	#print("event: "+str(event))
-	if event.type == InputEvent.MOUSE_BUTTON and event.button_index == BUTTON_LEFT:
-		if event.pressed:
-			hold_time = 0
-			shuffle_state = "pressed"
-		else: #left button released
-			
-			#execute shuffle
-			shuffle_state = "initialize"
-			#reset hold so hold time doesn't keep accumulating
+	if event is InputEventMouseButton:
+		
+		if event.get_button_index() == BUTTON_LEFT:
+			if event.is_pressed():
+				hold_time = 0
+				shuffle_state = "pressed"
+				print("hold time: "+str(hold_time))
+			else: #left button released
+				
+				#execute shuffle
+				shuffle_state = "initialize"
+				#reset hold so hold time doesn't keep accumulating
 
 
 
@@ -255,7 +278,7 @@ func _on_setup_list_warning_confirm_pressed():
 
 
 func _on_checkbox_crop_pressed():
-	if get_node("main frame/trim notice").is_hidden():
+	if get_node("main frame/trim notice").visible == false:
 		get_node("main frame/trim notice").show()
 		get_node("main frame/cutout").show()
 	else:
@@ -265,14 +288,16 @@ func _on_checkbox_crop_pressed():
 
 
 func _on_checkbox_name_pressed():
-	if get_node("main frame/result").is_hidden():
+	if get_node("settings frame/checkbox name").pressed == true:
+	#if get_node("main frame/result").visible == false:
 		get_node("main frame/result").show()
 	else:
 		get_node("main frame/result").hide()
 
 
 func _on_checkbox_number_pressed():
-	if get_node("main frame/number result").is_hidden():
+	if get_node("settings frame/checkbox number").pressed == true:
+	#if get_node("main frame/number result").visible == false:
 		get_node("main frame/number result").show()
 	else:
 		get_node("main frame/number result").hide()
